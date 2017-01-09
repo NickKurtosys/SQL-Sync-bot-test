47f56e6e8f8231cef34e5a32e41fe3efd9487836
Nickson Thanda <nickson.thanda@kurtosys.com>
2017-01-09 15:11:50 +0000

    Testing Sync bot test

--- server.js
+++ server.js
@@ -143,7 +143,7 @@ slapp.message('SYNC TABLE ALERT', (msg) => {
 })
 slapp.action('sync_ack', 'acknowledge', (msg, user) => {
   msg.respond(msg.body.response_url, `*Someone has acknowledged they are looking into the sync alert!*`)
-})
+},console.log(JSON.Stringify(msg))
 // start http server
 server.listen(port, (err) => {
   if (err) {
