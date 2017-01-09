75a90450fe287b3cae917f402e227cac526ba793
Nickson Thanda <nickson.thanda@kurtosys.com>
2017-01-09 15:04:37 +0000

    reverting earlier commits

--- server.js
+++ server.js
@@ -140,7 +140,7 @@ slapp.message('SYNC TABLE ALERT', (msg) => {
         }
     ]
 } )
-}, msg.meta.bot_id = null)
+})
 slapp.action('sync_ack', 'acknowledge', (msg, user) => {
   msg.respond(msg.body.response_url, `*Someone has acknowledged they are looking into the sync alert!*`)
 })
